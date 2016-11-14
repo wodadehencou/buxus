12268f2f6aae1ba1d77f457534cb757133556552
Jam Adams <wodadehencou@gmail.com>
2016-11-14 14:36:41 +0800

    add test for multi_thread, result is time of (sign + verify) [TODO] seperate sign & verify time

--- c_src/buxus_test.c
+++ c_src/buxus_test.c
@@ -4,6 +4,8 @@
 #ifdef LINUX_PERFORMANCE
 #define PERFORMANCE
 #include <sys/time.h>
+#include <pthread.h>
+#include <string.h>
 #endif
 #include "buxus_type.h"
 #include "bigint.h"
@@ -246,10 +248,113 @@ void sm2_performance_test() {
 	}
 }
 
+void* sm2_1000times_test(void* arg) {
+	UINT8 g [64] = {
+		0x32, 0xC4, 0xAE, 0x2C, 0x1F, 0x19, 0x81, 0x19,
+		0x5F, 0x99, 0x04, 0x46, 0x6A, 0x39, 0xC9, 0x94,
+		0x8F, 0xE3, 0x0B, 0xBF, 0xF2, 0x66, 0x0B, 0xE1,
+		0x71, 0x5A, 0x45, 0x89, 0x33, 0x4C, 0x74, 0xC7,
+		0xBC, 0x37, 0x36, 0xA2, 0xF4, 0xF6, 0x77, 0x9C,
+		0x59, 0xBD, 0xCE, 0xE3, 0x6B, 0x69, 0x21, 0x53,
+		0xD0, 0xA9, 0x87, 0x7C, 0xC6, 0x2A, 0x47, 0x40,
+		0x02, 0xDF, 0x32, 0xE5, 0x21, 0x39, 0xF0, 0xA0,
+	};
+	UINT8 p [32] = {
+		0xFF, 0xFF, 0xFF, 0xFE, 0xFF, 0xFF, 0xFF, 0xFF,
+		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+		0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00,
+		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+	};
+	UINT8 n [32] = {
+		0xFF, 0xFF, 0xFF, 0xFE, 0xFF, 0xFF, 0xFF, 0xFF,
+		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+		0x72, 0x03, 0xDF, 0x6B, 0x21, 0xC6, 0x05, 0x2B,
+		0x53, 0xBB, 0xF4, 0x09, 0x39, 0xD5, 0x41, 0x23,
+	};
+	UINT8 sk [32];
+	UINT8 pk [64];
+	UINT8 hash [32];
+	UINT8 random [32];
+
+	BIGINT big_sk [BIGINT_LEN];
+	AFFPOINT point_pk;
+	UINT8 sign_r [32];
+	UINT8 sign_s [32];
+	ECGROUP group;
+
+	int i, j, k;
+	int ret;
+	int flag = 0;
+
+	//set the group info
+	group_set_p(&group, p);
+	group_set_n(&group, n);
+	group_set_g(&group, g);
+
+	//generate public key
+	digitrand(sk, 32);
+	char2bigint(big_sk, sk);
+	basepoint_mul(&point_pk, big_sk, &group);
+	affpoint2char(pk, &point_pk);
+
+	for (j=0; j<1000; j++) {
+		digitrand(hash, 32);
+		digitrand(random, 32);
+		sm2_sign(hash, random, sk, sign_r, sign_s, &group);
+		ret = sm2_verify(hash, pk, sign_r, sign_s, &group);
+		if (ret != 0) {
+			printf("verify error\n");
+		}
+	}
+	return ((void*)0);
+}
+
+void sm2_multi_thread_test(int n) {
+	pthread_t* tid;
+	int err;
+	void* tret;
+	int i;
+	struct timeval start;
+	struct timeval end;
+	double d1;
+
+	tid = malloc(n*sizeof(pthread_t*));
+	gettimeofday(&start, NULL);
+
+	for (i=0; i<n; i++) {
+		err = pthread_create(tid+i, NULL, sm2_1000times_test, NULL);
+		if (err!=0) {
+			fprintf(stdout, "create thread error. n=%d, error info:%s\n", n, strerror(err));
+			exit(-1);
+		}
+	}
+	for (i=0; i<n; i++) {
+		err = pthread_join(*(tid+i), &tret);
+		if (err!=0) {
+			fprintf(stdout, "can't join with thread. n=%d, error info:%s\n", n, strerror(err));
+			exit(-1);
+		}
+	}
+
+	gettimeofday(&end, NULL);
+
+	d1 = end.tv_sec*1000000.0 - start.tv_sec*1000000.0 + (end.tv_usec - start.tv_usec);
+
+	fprintf(stdout,"### Test of %d thread \n", n);
+	fprintf(stdout,"--- total time is %f us \n", d1);
+	fprintf(stdout,"--- one (sign+verify) time is %f us \n", d1/(n*1000));
+
+	return;
+}
+
 void sm2_test() {
 	sm2_function_test();
 #ifdef PERFORMANCE
 	sm2_performance_test();
+	int i;
+	for (i=1; i<80; i++) {
+		sm2_multi_thread_test(i);
+	}
 #endif
 }
 
